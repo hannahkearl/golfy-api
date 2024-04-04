91ab644d88b50b761214ce88604a2852c3287495
Hannah Kearl <hannah@Hannahs-MacBook-Air.local>
2024-04-04 10:39:41 -0600

    Add endpoints

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import uuid
+
 from fastapi import FastAPI
+
+from models.course import Course
+from models.player import Player
 
 
 app = FastAPI()
 
+players: dict[uuid.UUID, Player] = {}
+courses: dict[uuid.UUID, Course] = {}
+
+@app.get("/courses")
+@app.post("/courses")
+@app.put("/courses/{course_id}")
+@app.delete("/courses/{course_id}")
+
+@app.get("/players")
+@app.post("/players")
+@app.put("/players/{player_id}")
+@app.delete("/players/{player_id}")
